98cc04827918743fee78dcaf6907239d1c0947af
binhfile <ngothanhbinh.pfiev@gmail.com>
2015-10-05 23:40:40 +0700

    del filesystem

--- fs/usr/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/dev/Desktop/PI/build/fs/lib"
-XML2_LIBS="-lxml2    -lm "
-XML2_INCLUDEDIR="-I/home/dev/Desktop/PI/build/fs/include/libxml2"
-MODULE_VERSION="xml2-2.7.3"
-
